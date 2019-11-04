10f1b1c70994b0ac5b84e5dfab5374bbbb7cdef1
creew <eklompus@student.21-school.ru>
2019-11-04 14:50:40 +0300

    start parsing map

--- lem-in.c
+++ lem-in.c
@@ -12,7 +12,27 @@
 
 #include "lem-in.h"
 
+int		read_input(int fd, t_lemin *lem)
+{
+	char	*s;
+	int		res;
+
+	if (get_next_line(fd, &s) < 0)
+		return (ERR_READ_ANTS_NUMBER);
+	if (ft_safe_atoi(s, &lem->number_ants) != FT_ATOI_OK)
+		return (ERR_WRONG_ANTS_NUMBER);
+	while ((res = get_next_line(fd, &s)))
+	{
+		if (res < 0)
+			return (ERR_GNL_ERROR);
+	}
+	return (RET_OK);
+}
+
 int		main(int ac, char *av[])
 {
+	t_lemin lem;
+	if (read_input(0, &lem) != RET_OK)
+		ft_putendl("Error");
 	return (0);
 }
--- lem-in.h
+++ lem-in.h
@@ -13,14 +13,25 @@
 #ifndef _LEM_IN_H
 # define _LEM_IN_H
 
-#include "libft.h"
+# include "libft.h"
+
+# define RET_OK					(0)
+# define ERR_READ_ANTS_NUMBER	(-1)
+# define ERR_WRONG_ANTS_NUMBER	(-2)
+# define ERR_GNL_ERROR			(-3)
+
+typedef struct	s_ldata
+{
+	char	*name;
+	int		x;
+	int		y;
+}				t_ldata;
 
 typedef struct	s_rooms
 {
 	struct s_rooms	*children;
 	struct s_rooms	*next;
-	int				x;
-	int				y;
+	t_ldata			*room;
 }				t_rooms;
 
 typedef struct	s_lemin
