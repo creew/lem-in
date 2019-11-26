e450cab0cae5a0ad4a1b93ae080509864592e798
creew <eklompus@student.21-school.ru>
2019-11-26 12:01:49 +0300

    last norme error

--- srcs/find_all_paths.c
+++ srcs/find_all_paths.c
@@ -52,7 +52,6 @@ static int		is_last_path_correct(t_adjlist *alist)
 				if (end->prev == NULL || end->room->cmd == LEM_CMD_START)
 					return (1);
 				end = end->prev;
-
 			}
 		}
 	}
@@ -69,14 +68,37 @@ static int		path_cmp(const void *d1, const void *d2)
 	return ((int)ft_array_size(p1) - (int)ft_array_size(p2));
 }
 
+static int		try_new_path(t_lemin *lem, int *prev_len, t_patharr **prev_sol,
+	t_patharr **one_sol)
+{
+	t_path		*newp;
+	int			len;
+	t_patharr	*newsol;
+
+	newp = get_last_shortest_path(lem->adjm);
+	print_path(newp);
+	newsol = recalc_values(&lem->rooms, *prev_sol, newp);
+	delete_path(&newp);
+	ft_bubble_sort(newsol->data, newsol->num_elems,
+		sizeof(*newsol->data), path_cmp);
+	len = calc_total_len(newsol, lem->num_ants);
+	if (len < *prev_len)
+	{
+		remove_all_paths(prev_sol);
+		*one_sol = newsol;
+		*prev_len = len;
+	}
+	else
+		return (0);
+	*prev_sol = newsol;
+	return (1);
+}
+
 t_result		find_all_paths(t_lemin *lem)
 {
 	t_patharr	*sol;
 	t_path		*path;
-	t_path		*newp;
 	int			prev_len;
-	int			len;
-	t_patharr	*newsol;
 	t_patharr	*onesol;
 	int			bf_updated;
 
@@ -91,25 +113,11 @@ t_result		find_all_paths(t_lemin *lem)
 		bf_updated = bellman_ford(lem->adjm);
 		if (bf_updated && is_last_path_correct(lem->adjm))
 		{
-			newp = get_last_shortest_path(lem->adjm);
-			print_path(newp);
-			newsol = recalc_values(&lem->rooms, sol, newp);
-			delete_path(&newp);
-			ft_bubble_sort(newsol->data, newsol->num_elems,
-				sizeof(*newsol->data), path_cmp);
-			len = calc_total_len(newsol, lem->num_ants);
-			if (len < prev_len)
-			{
-				remove_all_paths(&sol);
-				onesol = newsol;
-				prev_len = len;
-			}
-			else
+			if (!try_new_path(lem, &prev_len, &sol, &onesol))
 				break ;
-			sol = newsol;
 		}
 		else
-			break;
+			break ;
 	}
 	lem->paths = onesol;
 	return (RET_OK);
